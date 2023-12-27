b4d7d1dea54e6bfe4c908410d96f490f789cb223
Amee Nimavat <amee.code@gmail.com>
2023-12-27 17:57:36 +0530

    FIRST COMMIT

---
+++ App.js
@@ -0,0 +1,29 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text, SafeAreaView ,safe} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+function HomeScreen() {
+  return (
+    <safe style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </safe>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
